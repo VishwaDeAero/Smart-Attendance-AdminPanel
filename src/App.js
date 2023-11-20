b0830282762d0e4835ba506266f930afee779678
vsrimal <vishwadeaero@gmail.com>
2023-11-20 17:34:05 +0530

    basic mui app with sidebar and nav

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PersistentDrawerLeft from './components/Drawer';
+
+function App() {
+  return (
+    <div className='App'>
+      <PersistentDrawerLeft/>
+    </div>
+  );
+}
+
+export default App;
