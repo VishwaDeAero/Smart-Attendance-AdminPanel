268fc74323c943651d94e864c4d729c46d721172
vsrimal <vishwadeaero@gmail.com>
2024-01-19 11:46:25 +0530

    bug fix (UI Conflict)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Sample from './pages/sample';
 import Test from './pages/test';
+import PageNotFound from './pages/pageNotFound';
+import SignIn from './pages/signIn';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Sample/>} />
+        <Route path="/login" exact element={<SignIn/>} />
         <Route path="/test" exact element={<Test/>} />
+        <Route path="*" exact element={<PageNotFound/>} />
       </Routes>
     </Router>
   );
