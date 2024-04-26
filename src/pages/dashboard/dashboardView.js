e7cdf1793fb7d80f8723978e602355adc013ca25
vsrimal <vishwadeaero@gmail.com>
2024-04-27 00:18:51 +0530

    basic structure of dashboard ui

--- src/pages/dashboard/dashboardView.js
+++ src/pages/dashboard/dashboardView.js
@@ -8,16 +8,33 @@ import PieChart from '../../components/charts/PieChart'
 import BarChart from '../../components/charts/BarChart'
 
 const DashboardView = () => {  // State to hold fetched data
-    const attendanceData = [30, 40, 50, 45, 55];
-    const absentData = [5, 10, 8, 12, 6];
+    const lectureAttendance = {
+        labels: ['ITE3332', 'ITE2432', 'ITE4333', 'ITE3322'],
+        datasetLabels: ['Present', 'Absent', 'Late'], // Labels for each dataset
+        datasets: [
+          [20, 18, 22, 0], // Attendance data
+          [3, 1, 4, 0],     // Absent data
+          [0, 1, 2, 0],  // Late data
+          // Add more datasets as needed
+        ],
+    }
+
+    const totalAttendance = {
+        label: 'Test',
+        datasetLabels: ['Present', 'Absent', 'Late'], // Labels for each dataset
+        datasets: [
+          [60, 8, 3],  // Late data
+          // Add more datasets as needed
+        ],
+    }
+
     const chartData = {
         labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-        datasetLabels: ['Attendance', 'Absent', 'Late', 'Excused'], // Labels for each dataset
+        datasetLabels: ['Attendance', 'Absent', 'Late'], // Labels for each dataset
         datasets: [
           [30, 40, 50, 45, 55], // Attendance data
           [5, 10, 8, 12, 6],     // Absent data
           [10, 15, 20, 18, 25],  // Late data
-          [3, 6, 4, 5, 7],       // Excused data
           // Add more datasets as needed
         ],
       };
@@ -82,10 +99,10 @@ const DashboardView = () => {  // State to hold fetched data
                             <Typography fontWeight='bold' variant='h6'>Today Students' Performance</Typography>
                             <Grid2 container marginTop={4} spacing={2}>
                                 <Grid2 xs={12} md={8}>
-                                    <LineChart chartData={chartData} />
+                                    <LineChart id='lectureAttendance' chartData={lectureAttendance} />
                                 </Grid2>
                                 <Grid2 xs={12} md={4}>
-                                    <PieChart chartData={chartData} />
+                                    <PieChart id='totalAttendance' chartData={totalAttendance} />
                                 </Grid2>
                             </Grid2>
                         </CardContent>
