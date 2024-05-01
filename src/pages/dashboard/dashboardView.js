88356bc37dafe7cf6018d6d3dcf3a40a89601957
vsrimal <vishwadeaero@gmail.com>
2024-05-01 23:58:50 +0530

    Dashboard charts completed

--- src/pages/dashboard/dashboardView.js
+++ src/pages/dashboard/dashboardView.js
@@ -10,7 +10,7 @@ import { closeAlert, showLoading } from '../../utils/swal'
 import { Search } from '@mui/icons-material'
 import { DatePicker } from '@mui/x-date-pickers'
 import moment from 'moment'
-import { getDailyStats } from '../../services/analyticsService'
+import { getAttendanceStats, getDailyStats } from '../../services/analyticsService'
 
 const DashboardView = () => {
     const auth = useAuthHeader()
@@ -23,11 +23,17 @@ const DashboardView = () => {
         totalLectures: 0,
         dataSet: null,
     })
-    const [attendances, setAttendances] = useState([])
     const [formData, setFormData] = useState({
         startDate: null,
         endDate: null,
-    }) // State to hold fetched data
+    })
+    const [chartData, setChartData] = useState({
+        lectureAttendanceData: null,
+        subjectAttendance: null,
+        totalAttendance: null,
+    })
+    const [analyticsData, setAnalyticsData] = useState(null)
+
     const lectureAttendance = {
         labels: ['ITE3332', 'ITE2432', 'ITE4333', 'ITE3322'],
         datasetLabels: ['Present', 'Absent', 'Late'], // Labels for each dataset
@@ -40,15 +46,15 @@ const DashboardView = () => {
     }
 
     const totalAttendance = {
-        label: 'Test',
-        datasetLabels: ['Present', 'Absent', 'Late'], // Labels for each dataset
+        labels: ['Present', 'Absent'],
+        datasetLabels: ['Attendance'], // Labels for each dataset
         datasets: [
-            [60, 8, 3],  // Late data
+            [30, 40], // Attendance data
             // Add more datasets as needed
         ],
     }
 
-    const chartData = {
+    const chartData1 = {
         labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
         datasetLabels: ['Attendance', 'Absent', 'Late'], // Labels for each dataset
         datasets: [
@@ -61,17 +67,28 @@ const DashboardView = () => {
 
     useEffect(() => {
         showLoading()
-        fetchFilteredAttendances()
-    }, [formData])
+        fetchDailyStats();
+        fetchAttendanceData();
+    }, [])
 
     useEffect(() => {
-        showLoading()
-        fetchDailyStats();
-    }, [])
+        if (analyticsData) {
+            showLoading()
+            let lectureChartData = getLectureChartData();
+            let subjectChartData = getSubjectChartData();
+            let totalChartData = getTotalChartData();
+            setChartData({
+                lectureAttendanceData: lectureChartData,
+                subjectAttendance: subjectChartData,
+                totalAttendance: totalChartData
+            })
+            console.log("Chart Data:", chartData)
+        }
+    }, [analyticsData])
 
     const fetchDailyStats = async () => {
         try {
-            const dailyStatsResult = await getDailyStats( {}, auth)
+            const dailyStatsResult = await getDailyStats({}, auth)
             if (dailyStatsResult?.data) {
                 let attendedStudents = dailyStatsResult.data.dailyAttendance
                 let allStudents = dailyStatsResult.data.enrolledStudents
@@ -81,10 +98,10 @@ const DashboardView = () => {
                 let upcomingLectures = allLectures.length - lecturesHeld.length;
                 setDailyStats({
                     attendanceMarked: attendedStudents.length,
-                    attendanceAbsent: absentStudents,
-                    attendancePercentage: (attendedStudents.length/allStudents.length)*100,
+                    attendanceAbsent: (absentStudents > 0) ? absentStudents : 0,
+                    attendancePercentage: (allStudents.length == 0) ? (attendedStudents.length / allStudents.length) * 100 : 0,
                     lecturesHeld: lecturesHeld.length,
-                    lecturesUpcoming: upcomingLectures,
+                    lecturesUpcoming: (upcomingLectures > 0) ? upcomingLectures : 0,
                     totalLectures: allLectures.length,
                     dataSet: dailyStatsResult.data,
                 })
@@ -95,17 +112,134 @@ const DashboardView = () => {
         closeAlert()
     }
 
-    const fetchFilteredAttendances = async () => {
-        // try {
-        //     const attendancesData = await getStudentAttendance(attendanceData, auth)
-        //     if (attendancesData?.data) {
-        //         setAttendances(attendancesData.data)
-        //     }
-        // } catch (error) {
-        //     console.error('Error fetching attendances:', error)
-        // }
+    const fetchAttendanceData = async () => {
+        try {
+            const attendanceStats = await getAttendanceStats(formData, auth)
+            console.log(attendanceStats)
+            if (attendanceStats?.data) {
+                let data = attendanceStats.data
+                setAnalyticsData(data)
+            }
+        } catch (error) {
+            console.error('Error fetching attendances:', error)
+        }
         closeAlert()
     }
+
+    const getTotalChartData = () => {
+
+        const { enrolledLectureData, attendanceData } = analyticsData
+
+        let totalAttendanceData = {
+            labels: ['Present', 'Absent'],
+            datasetLabels: ['Attendance'], // Labels for each dataset
+            datasets: [
+                [], // 'Present', 'Absent'
+                // Add more datasets as needed
+            ],
+        }
+
+        let allAttendance = attendanceData.length;
+        let enrolledStudents = 0;
+
+        // Iterate over the enrolledLectureData to calculate total enrolled students
+        enrolledLectureData.forEach((lecture) => {
+            enrolledStudents += lecture.enrolledStudents.length
+        });
+
+        totalAttendanceData.datasets[0][0] = allAttendance;
+        totalAttendanceData.datasets[0][1] = (enrolledStudents - allAttendance);
+
+        closeAlert()
+        return totalAttendanceData;
+    }
+
+    const getLectureChartData = () => {
+
+        const { enrolledLectureData, attendanceData } = analyticsData
+
+        let lectureAttendance = {
+            labels: [],
+            datasetLabels: ['Present', 'Absent'],
+            datasets: [[], []],
+        };
+
+        // Create a map to store attendance data for each lecture
+        const lectureAttendanceMap = new Map();
+
+        // Iterate over the enrolledLectureData to populate labels and initialize attendance map
+        enrolledLectureData.forEach((lecture) => {
+            const label = `${lecture.subject.code} - ${moment(lecture.scheduledAt).format('YYYY/MM/DD')}`;
+            lectureAttendance.labels.push(label);
+            lectureAttendanceMap.set(lecture.id, { attended: 0, total: lecture.enrolledStudents.length });
+        });
+
+        // Iterate over attendance data to calculate attendance and absent counts for each lecture
+        attendanceData.forEach((attendance) => {
+            const lectureId = attendance.lectureId;
+            if (lectureAttendanceMap.has(lectureId)) {
+                const attendanceInfo = lectureAttendanceMap.get(lectureId);
+                attendanceInfo.attended++;
+            }
+        });
+
+        // Generate data points for each dataset
+        lectureAttendanceMap.forEach((attendanceInfo) => {
+            lectureAttendance.datasets[0].push(attendanceInfo.attended);
+            lectureAttendance.datasets[1].push(attendanceInfo.total - attendanceInfo.attended);
+        });
+
+        closeAlert()
+        return lectureAttendance;
+    };
+
+    const getSubjectChartData = () => {
+
+        const { enrolledLectureData, attendanceData } = analyticsData
+
+        // Initialize an empty object to store attendance and absent data for each subject
+        const attendanceMap = {};
+        const absentMap = {};
+
+        // Iterate over each lecture in the enrolled lecture data
+        enrolledLectureData.forEach(lecture => {
+            const subjectCode = lecture.subject.code;
+
+            // Initialize arrays for attendance and absent data if not already created
+            if (!attendanceMap[subjectCode]) {
+                attendanceMap[subjectCode] = [];
+                absentMap[subjectCode] = [];
+            }
+
+            // Find attendance data for the current lecture
+            const lectureAttendance = attendanceData.filter(attendance => attendance.lectureId === lecture.id);
+
+            // Calculate attendance and absent data for the current lecture
+            const totalEnrolledStudents = lecture.enrolledStudents.length;
+            const attendedStudents = lectureAttendance.length;
+            const absentStudents = totalEnrolledStudents - attendedStudents;
+
+            // Push the attendance and absent data to the corresponding arrays
+            attendanceMap[subjectCode].push(attendedStudents);
+            absentMap[subjectCode].push(absentStudents);
+        });
+
+        // Extract subject codes and datasets from the attendance and absent maps
+        const labels = Object.keys(attendanceMap);
+        const attendanceDatasets = Object.values(attendanceMap).flat();
+        const absentDatasets = Object.values(absentMap).flat();
+
+        // Construct the chart data object
+        const chartData = {
+            labels: labels,
+            datasetLabels: ['Attendance', 'Absent'],
+            datasets: [attendanceDatasets, absentDatasets],
+        };
+
+        closeAlert()
+        return chartData;
+    };
+
 
     return (
         <MainLayout>
@@ -203,22 +337,24 @@ const DashboardView = () => {
                         (e) => {
                             e.stopPropagation()
                             showLoading()
-                            fetchFilteredAttendances()
+                            fetchAttendanceData()
                         }
                     }>
                         Search
                     </Button>
                 </Grid2>
             </Grid2>
-            <Grid2 marginTop={2}  xs={12}>
+            <Grid2 marginTop={2} xs={12}>
                 <Card>
                     <CardContent>
                         <Grid2 container marginTop={4} spacing={2}>
                             <Grid2 xs={12} md={8}>
-                                <LineChart id='lectureAttendance' chartData={lectureAttendance} />
+                                <Typography fontWeight='bold' variant='h6'>Lecture Attendance</Typography>
+                                {chartData.lectureAttendanceData && <LineChart id='lectureAttendance' chartData={chartData.lectureAttendanceData} />}
                             </Grid2>
                             <Grid2 xs={12} md={4}>
-                                <PieChart id='totalAttendance' chartData={totalAttendance} />
+                                <Typography fontWeight='bold' variant='h6' align='center'>Total Attendance</Typography>
+                                {chartData.totalAttendance && <PieChart id='totalAttendance' chartData={chartData.totalAttendance} />}
                             </Grid2>
                         </Grid2>
                     </CardContent>
@@ -229,10 +365,10 @@ const DashboardView = () => {
                 <Grid2 xs={12}>
                     <Card>
                         <CardContent>
-                            <Typography fontWeight='bold' variant='h6'>Lecture Attendance</Typography>
+                            <Typography fontWeight='bold' variant='h6'>Subject Attendance</Typography>
                             <Grid2 container marginTop={4} spacing={2}>
                                 <Grid2 xs={12}>
-                                    <BarChart chartData={chartData} />
+                                    {chartData.subjectAttendance && <BarChart chartData={chartData.subjectAttendance} />}
                                 </Grid2>
                             </Grid2>
                         </CardContent>
